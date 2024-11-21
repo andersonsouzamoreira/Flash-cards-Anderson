84ddaa7e9b42cda75df51e0e3ca377df2d94fc0e
andersonsouzamoreira <147444027+andersonsouzamoreira@users.noreply.github.com>
2024-11-21 14:02:16 -0300

    21/11

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
-    cartao.className = 'cartao'; 
+    cartao.className = 'cartao';
 
     cartao.innerHTML = ` 
                 <div class="cartao__conteudo">
@@ -12,7 +12,17 @@ function criaCartao(categoria, pergunta, resposta){
                     <div class="cartao__conteudo__resposta">
                         <p>${resposta}</p>
                     </div>
-                </div> `
+                </div> 
+                `
+
+    let respostaEstaVisivel = false;
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
+    cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
